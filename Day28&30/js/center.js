f046606fed542f7bd3eff1571ade5576e126a453
太甜 <tz2489126@vip.qq.com>
2018-06-21 16:54:37 +0800

    半成品

--- Day28&30/js/center.js
+++ Day28&30/js/center.js
@@ -7,27 +7,59 @@ function emailPrompt() {
     addNode();
 }
 
-// function getEmail() {
-//     return email.value.replace(/(^\s+)|(\s+$)/g, "");
-// }
-
 function getEmail() {
-    let x = email.value.replace(/(^\s+)|(\s+$)/g, ""),
-        y = x.indexOf("@");
-    if (y !== -1) {
-        return x.slice(0, y);
-    } else {
-        return x;
-    }
+    return email.value.replace(/(^\s+)|(\s+$)/g, "");
 }
 
-function getNode() {
-    let arr = [];
-    str = getEmail();
 
-    if (str.length > 0) {
+// function getEmail() {
+//     let x = email.value.replace(/(^\s+)|(\s+$)/g, ""),
+//         y = x.indexOf("@");
+//     if (y !== -1) {
+//         return x.slice(0, y);
+//     } else {
+//         return x;
+//     }
+// }
+//  需求2
+
+// function getNode() {
+//     let arr = [];
+//     str = getEmail();
+
+//     if (str.length > 0) {
+//         for (let i of postfixList) {
+//             arr.push(str + "@" + i);
+//         }
+//         return arr;
+//     }
+// }
+
+function getNode() {
+    str = getEmail();
+    let arr = [],
+        y = str.indexOf("@");
+    if (y !== -1) {
+        let strLeft = str.slice(0, y),
+            strRight = str.slice(y + 1);
+        for (let i of postfixList) {
+            if (i.indexOf(strRight) === 0) {
+                arr.push(strLeft + "@" + i);
+            }
+        }
+
+        if (arr.length > 0) {
+            return arr;
+        } else {
+            for (let i of postfixList) {
+                arr.push(strLeft + "@" + i);
+            }
+            return arr;
+        }
+    } else {
         for (let i of postfixList) {
             arr.push(str + "@" + i);
+            // console.log(arr);
         }
         return arr;
     }
@@ -57,7 +89,15 @@ function emailSugHide() {
     }
 }
 
+function listenMouse(ev) {
+    let target = ev.target;
+    if (target.nodeName.toLowerCase() === "li") {
+        email.value = target.innerText;
+        emailSug.style.setProperty("display", "none");
+    }
+}
 
 
 
 email.addEventListener("input", emailPrompt);
+emailSug.addEventListener("click", listenMouse);
